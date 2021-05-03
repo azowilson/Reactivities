9fdd007a16e0b455377887d90667b1294afd298c
azowilson <42116847+azowilson@users.noreply.github.com>
2021-05-03 15:40:53 +0800

    End of section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { act } from 'react-dom/test-utils';
+import { Header, List } from 'semantic-ui-react';
+
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    // axios is promoise base
+    axios.get('http://localhost:5000/api/activities').then(
+      response => {
+        console.log(response);
+        setActivities(response.data);
+      }
+    )
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+       <List>
+          {activities.map((activity: any) => (
+            // looping
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
+    </div>
+  );
+}
+
+export default App;
